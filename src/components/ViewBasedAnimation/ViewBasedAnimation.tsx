bd79810a3a17f0a8e72702c4f24a33ad8c81a457
Yuchen Liu <lyc990312@hotmail.com>
2024-08-01 15:03:36 +1000

    Remove the unused import

--- src/components/ViewBasedAnimation/ViewBasedAnimation.tsx
+++ src/components/ViewBasedAnimation/ViewBasedAnimation.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { motion, useInView } from 'framer-motion'
 
 export default function ViewBasedAnimation() {
