8e74d6ab634eec5a577967456c64b9a8f6c749e0
Yuchen Liu <lyc990312@hotmail.com>
2024-08-01 14:59:15 +1000

    Finished the view based animation

--- src/components/ViewBasedAnimation/ViewBasedAnimation.tsx
+++ src/components/ViewBasedAnimation/ViewBasedAnimation.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React, { useEffect, useRef } from 'react'
+import { motion, useInView } from 'framer-motion'
 
 export default function ViewBasedAnimation() {
+
+  const ref = useRef(null)   // add the ref to the target div
+  const isInView = useInView(ref, { once: true })   // use the useInView to detect if it is in view
+
+  useEffect(() => {
+    console.log("is in view", isInView)
+  }, [isInView])
+
   return (
     <div>
       <div style={{height: "150vh"}} />
@@ -10,6 +18,14 @@ export default function ViewBasedAnimation() {
         initial={{opacity: 0}}
         whileInView={{opacity: 1}}
         transition={{duration: 1}}
+      />
+      <div 
+      ref={ref}  // add the target ref in here
+      style={{
+        height: "100vh",
+        background: isInView ? "green" : "blue" ,
+        transition: "1s background"
+      }}
       />
     </div>
   )
