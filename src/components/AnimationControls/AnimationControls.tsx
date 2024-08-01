ef28608db3699386af08a60190d1fb0a4d40cf4a
Yuchen Liu <lyc990312@hotmail.com>
2024-08-01 14:38:40 +1000

    Finifh the animation controls

--- src/components/AnimationControls/AnimationControls.tsx
+++ src/components/AnimationControls/AnimationControls.tsx
@@ -1,18 +1,33 @@
-import { motion } from "framer-motion"
+import { motion, useAnimationControls } from "framer-motion"
 
 export default function AnimationControls() {
-  return (
-    <div style={{marginTop:200}} >
-      <button>Flip it</button>
-      <motion.div
-      style={{
-        width: 150,
-        height: 150,
-        backgroundColor:"black",
-        
-      }}
-      >
+  const controls = useAnimationControls()
+  const handleClick = () => {
+    controls.start("flop")
 
+  }
+
+  return (
+    <div style={{ marginTop: 200 }} >
+      <button onClick={handleClick} >Flip it</button>
+      <motion.div
+        style={{
+          width: 150,
+          height: 150,
+          backgroundColor: "black",
+        }}
+        variants={{    // custom variants which can be used in other attribute
+          initial: {
+            rotate: "0deg"
+          },
+          flop: {
+            rotate: "180deg"
+          }
+        }}
+        // whileHover="flop"   // eg
+        initial = "initial"
+        animate = {controls}
+      >
       </motion.div>
     </div>
   )
