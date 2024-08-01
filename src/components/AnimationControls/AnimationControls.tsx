f6a5818008841a4fc7d10d6ed968b828a8a047d9
Yuchen Liu <lyc990312@hotmail.com>
2024-08-01 13:02:37 +1000

    Add  the animation controls components

---
+++ src/components/AnimationControls/AnimationControls.tsx
@@ -0,0 +1,12 @@
+import { motion } from "framer-motion"
+
+export default function AnimationControls() {
+  return (
+    <div>
+      <button>Flip it</button>
+      <motion.div>
+
+      </motion.div>
+    </div>
+  )
+}
